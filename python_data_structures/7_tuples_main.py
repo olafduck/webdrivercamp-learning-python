69998941a19a46759cf21b1ff5b766ffb0644e40
olafduck <alexpesniak@gmail.com>
2024-10-06 23:40:53 -0400

    removed incomplete 2.1 files

--- python_data_structures/7_tuples_main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-tuple_addition = __import__('7_tuple_addition').tuple_addition
-first_arg, second_arg = (1, 99), (-1, 1)
-result = tuple_addition(first_arg, second_arg)
-print(result)
-print(tuple_addition(first_arg, (1, )))
-print(tuple_addition((1, 2, 3, 4), (-1, -2, -3, -4)))
-print(tuple_addition((), first_arg))
-print(tuple_addition())
